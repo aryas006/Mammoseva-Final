ffd09885e908a944130e895578d6d3414c0357b6
Aarya Paradkar <aaru7276@gmail.com>
2024-05-30 01:17:09 +0530

    server config changed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         console.log('MongoDB connected')
     } catch (error) {
         console.error(error)
